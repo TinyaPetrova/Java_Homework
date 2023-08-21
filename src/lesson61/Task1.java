78dc1cf0427b4b7418fb3f4e7feb9fff7bc690ad
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-21 17:07:07 +0200

    feat: add solution

--- src/lesson61/Task1.java
+++ src/lesson61/Task1.java
@@ -1,10 +1,33 @@
 package lesson61;
 
+import java.util.Scanner;
+
 /**
  * Находим минимальное чётное число в последовательности положительных чисел
  * или выводим -1, если такого числа не существует.
  * Оцениваем временную и пространственную сложность алгоритма.
  */
-public class Task1 {
+public class Task1 {                                 // Time:            Space:
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Количество чисел -> ");
+    int amount = scanner.nextInt();
+    int minEvenNum = Integer.MAX_VALUE;
+    boolean isEvenFound = false;
+    for (int i = 0; i < amount; i++) {
+      System.out.print("Число " + (i + 1) + " -> ");
+      int num = scanner.nextInt();
+      if (num > 0 && num % 2 == 0) {
+        isEvenFound = true;
+        minEvenNum = Math.min(minEvenNum, num);
+      }
+    }
+    if (isEvenFound) {
+      System.out.println(minEvenNum);
+    } else {
+      System.out.println(-1);
+    }
+  }
 
 }
