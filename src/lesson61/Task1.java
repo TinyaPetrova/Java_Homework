529e4aa9174fee707ec37d347d6210de3a5b10a9
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-21 17:22:38 +0200

    feat: add complexity evaluation

--- src/lesson61/Task1.java
+++ src/lesson61/Task1.java
@@ -7,26 +7,26 @@ import java.util.Scanner;
  * или выводим -1, если такого числа не существует.
  * Оцениваем временную и пространственную сложность алгоритма.
  */
-public class Task1 {                                 // Time:            Space:
+public class Task1 {                                   // Time: O(n)           Space: O(1)
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Количество чисел -> ");
-    int amount = scanner.nextInt();
-    int minEvenNum = Integer.MAX_VALUE;
-    boolean isEvenFound = false;
-    for (int i = 0; i < amount; i++) {
-      System.out.print("Число " + (i + 1) + " -> ");
-      int num = scanner.nextInt();
-      if (num > 0 && num % 2 == 0) {
-        isEvenFound = true;
-        minEvenNum = Math.min(minEvenNum, num);
+    Scanner scanner = new Scanner(System.in);          // 0(1)                 O(1)
+    System.out.print("Количество чисел -> ");          // 0(1)                 O(1)
+    int amount = scanner.nextInt();                    // 0(1)                 O(1)
+    int minEvenNum = Integer.MAX_VALUE;                // 0(1)                 O(1)
+    boolean isEvenFound = false;                       // 0(1)                 O(1)
+    for (int i = 0; i < amount; i++) {                 // 0(n)                 O(1)
+      System.out.print("Число " + (i + 1) + " -> ");   // 0(1)                 O(1)
+      int num = scanner.nextInt();                     // 0(1)                 O(1)
+      if (num > 0 && num % 2 == 0) {                   // 0(1)                 O(1)
+        isEvenFound = true;                            // 0(1)                 O(1)
+        minEvenNum = Math.min(minEvenNum, num);        // 0(1)                 O(1)
       }
     }
-    if (isEvenFound) {
-      System.out.println(minEvenNum);
+    if (isEvenFound) {                                 // 0(1)
+      System.out.println(minEvenNum);                  // 0(1)
     } else {
-      System.out.println(-1);
+      System.out.println(-1);                          // 0(1)
     }
   }
 
