fdbce5daa0c468e7c51174a5f3f5e8f8e73f7076
Ustinya Petrova <ust.petrova@gmail.com>
2023-08-25 18:26:13 +0200

    feat: add solution

--- src/lesson64/Main.java
+++ src/lesson64/Main.java
@@ -1,11 +1,36 @@
 package lesson64;
 
+import java.util.*;
+
 /**
- * Задача:
- * Используйте класс Book из домашнего задания к уроку 53 (см. Book.java), но без интерфейса Comparable.
- * Создайте список книг.
- * Отсортируйте книги по автору, (при одинаковых авторах - по названиям) и выведите получившийся список с использованием потоков.
+ * Задача: Используйте класс Book из домашнего задания к уроку 53 (см. Book.java), но без интерфейса
+ * Comparable. Создайте список книг. Отсортируйте книги по автору, (при одинаковых авторах - по
+ * названиям) и выведите получившийся список с использованием потоков.
  */
+
 public class Main {
 
+  public static void main(String[] args) {
+    List<Book> books = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Введите количество книг: ");
+    int numBooks = scanner.nextInt();
+    scanner.nextLine();
+    for (int i = 0; i < numBooks; i++) {
+      System.out.println("Книга " + (i + 1));
+      System.out.print("Введите автора: ");
+      String author = scanner.nextLine();
+      System.out.print("Введите название: ");
+      String title = scanner.nextLine();
+      System.out.print("Введите кол-во страниц: ");
+      int pages = scanner.nextInt();
+      scanner.nextLine();
+      books.add(new Book(author, title, pages));
+    }
+
+    books.stream()
+               .sorted(Comparator.comparing(Book::getAuthor).thenComparing(Book::getTitle))
+               .forEach(System.out::println);
+  }
 }
+
