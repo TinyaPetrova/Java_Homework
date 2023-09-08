aee4f1aa279fd4cbee0a33d2901bec4a490b2a4a
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-08 16:40:37 +0200

    feat: add tests

--- src/lesson62/Task1Tests.java
+++ src/lesson62/Task1Tests.java
@@ -1,7 +1,27 @@
 package lesson62;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class Task1Tests {
 
+  @Test
+  public void testReverseCommon() {
+    // arrange
+    int source = 123;
+
+    //act
+    String res = Task1.reverse(source);
+
+    //assert
+    assertEquals("321", res);
+  }
+
+  @Test
+  public void testReverseZero() {
+    int source = 0;
+    String res = Task1.reverse(source);
+    assertEquals("321", res);
+  }
 }
