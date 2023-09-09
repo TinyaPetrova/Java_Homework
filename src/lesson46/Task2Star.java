c0bf1a96508f844d2ad47b617c24b5b0ea1348a5
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-09 11:09:19 +0200

    feat: add code to read number of words and fill the dictionary

--- src/lesson46/Task2Star.java
+++ src/lesson46/Task2Star.java
@@ -1,5 +1,9 @@
 package lesson46;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task2Star {
 
   /**
@@ -13,6 +17,22 @@ public class Task2Star {
    * @param args
    */
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    Map<String, String> dictionary = new HashMap<>();
+    System.out.print("Введите количество записей: ");
+    int count = scanner.nextInt();
+    scanner.nextLine();
+    for (int i = 0; i < count; i++) {
+      System.out.print("Введите фамилию и имя через пробел: ");
+      String input = scanner.nextLine();
+      String[] parts = input.split(" ");
+      if (parts.length == 2) {
+        String lastName = parts[0];
+        String firstName = parts[1];
+        dictionary.put(lastName, firstName);
+      } else {
+        System.out.println("\u001B[31mНеправильный формат ввода, запись не добавлена!\u001B[0m");
+      }
+    }
   }
 }
