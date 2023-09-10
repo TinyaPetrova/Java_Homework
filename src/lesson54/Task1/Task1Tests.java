74c1f72a1d443741cc40e58853a5010bbba1c231
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-10 13:56:39 +0200

    feat: improve the first test testBookComparator()

--- src/lesson54/Task1/Task1Tests.java
+++ src/lesson54/Task1/Task1Tests.java
@@ -2,6 +2,7 @@ package lesson54.Task1;
 
 import static java.util.Arrays.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,12 +17,12 @@ public class Task1Tests {
     Book book3 = new Book("B", "2", 250);
     Book[] books = {book1, book2, book3};
     BookComparator bookComparator = new BookComparator();
-    sort(books, bookComparator);
+    Arrays.sort(books, bookComparator);
 
-    assertEquals("B", books[0].getAuthor());
-    assertEquals("2", books[0].getTitle());
-    assertEquals("C", books[1].getAuthor());
-    assertEquals("1", books[1].getTitle());
+    assertEquals("C", books[0].getAuthor());
+    assertEquals("1", books[0].getTitle());
+    assertEquals("B", books[1].getAuthor());
+    assertEquals("2", books[1].getTitle());
     assertEquals("A", books[2].getAuthor());
     assertEquals("3", books[2].getTitle());
   }
@@ -47,5 +48,16 @@ public class Task1Tests {
     sort(books, bookComparator);
 
     assertEquals(0, books.length);
+  }
+
+  @Test
+  public void testBookComparatorWithSingleBook() {
+    Book book1 = new Book("A", "1", 100);
+    Book[] books = {book1};
+    BookComparator bookComparator = new BookComparator();
+    sort(books, bookComparator);
+
+    assertEquals("A", books[0].getAuthor());
+    assertEquals("1", books[0].getTitle());
   }
 }
