49d5227d694226888b2046a995d8f1080753a8d7
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-10 13:29:52 +0200

    feat: add PagesComparator to Task2 and create Book class objects to check result of comparator's work

--- src/lesson54/Task2Star/Task2.java
+++ src/lesson54/Task2Star/Task2.java
@@ -1,5 +1,7 @@
 package lesson54.Task2Star;
 
+import java.util.Arrays;
+
 /**
  * Создайте компаратор, который позволит сортировать книги
  * по убыванию количества страниц, а при совпадающем объёме - по названиям
@@ -8,7 +10,14 @@ package lesson54.Task2Star;
 public class Task2 {
 
   public static void main(String[] args) {
-
+    Book book1 = new Book("A", "3", 300);
+    Book book2 = new Book("C", "1", 200);
+    Book book3 = new Book("B", "2", 250);
+    Book[] books = {book1, book2, book3};
+    PagesComparator pagesComparator = new PagesComparator();
+    Arrays.sort(books, pagesComparator);
+    for (Book book : books) {
+      System.out.println(book);
+    }
   }
-
 }
