b8d4b9d89f265cf58d36de8bacbd987eae8559c4
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-10 12:39:48 +0200

    feat: add class structure for Book

---
+++ src/lesson53/Book.java
@@ -0,0 +1,40 @@
+package lesson53;
+
+public class Book implements Comparable<Book> {
+
+  private String author;
+  private String title;
+  private int pages;
+
+  public Book(String author, String title, int pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getPages() {
+    return pages;
+  }
+
+  @Override
+  public int compareTo(Book o) {
+    return 0;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+        "author='" + author + '\'' +
+        ", title='" + title + '\'' +
+        ", pages=" + pages +
+        '}';
+  }
+}
