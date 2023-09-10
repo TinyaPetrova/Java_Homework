348bcf5a2f0bb55e58b70b586e7f1ecb24d19811
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-10 12:54:57 +0200

    feat: improve docstring with task description

--- src/lesson47/Task1.java
+++ src/lesson47/Task1.java
@@ -4,31 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * На вход программе подаётся одна строка текста.
+ * Необходимо посчитать частоту появления разных символов в строке и вывести результат.
+ * Пример ввода:
+ * Lorem ipsum dolor sit amet.
+ * Пример вывода (порядок может быть другим):
+ * l: 2
+ * o: 3
+ * r: 2
+ * e: 2
+ * m: 3
+ *  : 4
+ * i: 2
+ * p: 1
+ * s: 2
+ * u: 1
+ * d: 1
+ * t: 2
+ * a: 1
+ * .: 1
+ */
 public class Task1 {
 
-  /**
-   * На вход программе подаётся одна строка текста.
-   * Необходимо посчитать частоту появления разных символов в строке и вывести результат.
-   * Пример ввода:
-   * Lorem ipsum dolor sit amet.
-   * Пример вывода (порядок может быть другим):
-   * l: 2
-   * o: 3
-   * r: 2
-   * e: 2
-   * m: 3
-   *  : 4
-   * i: 2
-   * p: 1
-   * s: 2
-   * u: 1
-   * d: 1
-   * t: 2
-   * a: 1
-   * .: 1
-   *
-   * @param args аргументы командной строки
-   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
