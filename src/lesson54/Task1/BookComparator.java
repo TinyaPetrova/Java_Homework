e5441b7d8e1b913c00c0cff3b8ef2641692eb8f5
Ustinya Petrova <ust.petrova@gmail.com>
2023-09-10 12:59:22 +0200

    feat: redefine method compare()

--- src/lesson54/Task1/BookComparator.java
+++ src/lesson54/Task1/BookComparator.java
@@ -1,5 +1,14 @@
 package lesson54.Task1;
 
-public class BookComparator {
+import java.util.Comparator;
 
+public class BookComparator implements Comparator<Book> {
+  @Override
+  public int compare(Book book1, Book book2) {
+    int titleComparison = book1.getTitle().compareTo(book2.getTitle());
+    if (titleComparison != 0) {
+      return titleComparison;
+    }
+    return book1.getAuthor().compareTo(book2.getAuthor());
+  }
 }
